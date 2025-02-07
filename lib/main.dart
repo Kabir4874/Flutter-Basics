ae229e9dea8a8a430d8d63d90ce18371e09e2a25
kabir4874 <kabir.cse.bd@gmail.com>
2025-02-07 21:48:28 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -42,52 +42,19 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body:SingleChildScrollView(
-        child:  Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(bottom: 20),
-              height: 200,
-              width: 200,
-              color: Colors.lightGreen,
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 20),
-              height: 200,
-              width: 200,
-              color: Colors.lightGreen,
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 20),
-              height: 200,
-              width: 200,
-              color: Colors.lightGreen,
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 20),
-              height: 200,
-              width: 200,
-              color: Colors.lightGreen,
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 20),
-              height: 200,
-              width: 200,
-              color: Colors.lightGreen,
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 20),
-              height: 200,
-              width: 200,
-              color: Colors.lightGreen,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      )
-    );
+        body: Center(
+            child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Text(
+              'One',
+              style: TextStyle(color: Colors.red),
+            );
+          },
+          itemCount: 5,
+        )));
   }
 }
