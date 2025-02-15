4339b75f0bc638465a64cd1c0737d89296a84c76
kabir4874 <kabir.cse.bd@gmail.com>
2025-02-15 21:43:48 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -42,22 +42,23 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Card(
-            elevation: 5,
-            shadowColor: Colors.blue,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Hello World',
-                style: TextStyle(fontSize: 25),
-              ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Card(
+          elevation: 5,
+          shadowColor: Colors.blue,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Hello World',
+              style: TextStyle(fontSize: 30),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
