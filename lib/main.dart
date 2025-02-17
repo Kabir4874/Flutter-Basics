d297d6d069d10f83531be8d136a71d2ffc4c564a
kabir4874 <kabir.cse.bd@gmail.com>
2025-02-17 21:10:59 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -43,11 +43,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: ElevatedButton(onPressed: (){}, child: RoundedButton())
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: RoundedButton());
   }
 }
