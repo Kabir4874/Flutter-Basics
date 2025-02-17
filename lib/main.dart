7500959fc847685dfb4706bc738b673ef30bd2a7
kabir4874 <kabir.cse.bd@gmail.com>
2025-02-17 21:26:31 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -47,11 +47,16 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: RoundedButton(
-            btnName: "Login",
-            icon: Icon(Icons.lock),
-            callBack: () {
-              print("Logged In!!");
-            }, textStyle: TextStyle(fontSize: 25),));
+        body: Center(
+          child: Container(
+            width: 100,
+            child: RoundedButton(
+                btnName: "Login",
+                icon: Icon(Icons.lock),
+                callBack: () {
+                  print("Logged In!!");
+                }, textStyle: TextStyle(fontSize: 25),),
+          ),
+        ));
   }
 }
