4c06d77b7d5e02b92fe2e8c5da625ad0b0d1246c
kabir4874 <kabir.cse.bd@gmail.com>
2025-02-23 22:37:56 +0600

    update

--- lib/IntroPage.dart
+++ lib/IntroPage.dart
@@ -6,7 +6,7 @@ class IntroPage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Intro Screen"),
+        title: Text("Intro"),
       ),
       body: Text('Intro Page') ,
     );
