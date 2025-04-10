5a4da36b1d810ee834e575f34e6acd670c2ce491
kabir4874 <kabir.cse.bd@gmail.com>
2025-04-10 20:16:50 +0600

    simple design

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,122 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  var no1Controller = TextEditingController();
-  var no2Controller = TextEditingController();
-  var result = "";
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Colors.red,
+        title: Text(
+          'Mobile Application',
+          style: TextStyle(
+              fontSize: 35, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        actions: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: IconButton(
+              icon: Icon(
+                Icons.person,
+                color: Colors.red,
+                size: 30,
+              ),
+              onPressed: () {
+                print('Profile icon clicked');
+              },
+            ),
+          ),
+        ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.business), label: "Business"),
-          BottomNavigationBarItem(icon: Icon(Icons.school), label: "School"),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(color: Colors.redAccent),
+              child: DrawerHeader(
+                child: Text(
+                  'My App',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  height: 200,
+                ),
+              ),
+              Container(width: 20, height: 200, color: Colors.grey),
+              Expanded(
+                child: Container(
+                  color: Colors.greenAccent,
+                  height: 200,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            height: 300,
+            child: Image.network(
+              'https://images.pexels.com/photos/18261149/pexels-photo-18261149.jpeg?cs=srgb&dl=pexels-manishjangid-18261149.jpg&fm=jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  height: 200,
+                ),
+              ),
+              Container(width: 20, height: 200, color: Colors.grey),
+              Expanded(
+                child: Container(
+                  color: Colors.greenAccent,
+                  height: 200,
+                ),
+              ),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print('Button Working');
+            },
+            child: Text('Click'),
+          ),
         ],
       ),
     );
